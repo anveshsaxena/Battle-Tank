cbf00c3345c28be8030898538db1e2b2f6236b9b
Anvesh Saxena <anvsh@live.in>
2022-05-06 11:30:04 +0800

    Initialized MVC architecture

---
+++ Assets/Scripts/TankModel.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class TankModel
+{
+    private TankController tankController;
+
+    public void SetTankController(TankController _tankController)
+    {
+        tankController = _tankController;
+    }
+}
---
+++ Assets/Scripts/TankSpawner.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankSpawner : MonoBehaviour
+{
+    public TankView tankView;
+    // Start is called before the first frame update
+    void Start()
+    {
+        CreateTank();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void CreateTank()
+    {
+        TankModel tankModel = new TankModel();
+        TankController tankController = new TankController(tankModel, tankView);
+    }
+}
