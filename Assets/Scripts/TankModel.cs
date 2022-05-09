7fb56710c25ab1b6accc9e7dbadfc1218a248052
Anvesh Saxena <anvsh@live.in>
2022-05-09 22:26:42 +0800

    Tank and Camera movement

--- Assets/Scripts/TankModel.cs
+++ Assets/Scripts/TankModel.cs
@@ -4,6 +4,14 @@ public class TankModel
 {
     private TankController tankController;
 
+    public float movementSpeed;
+    public float rotationSpeed;
+
+    public TankModel(float _movement, float _rotation)
+    {
+        movementSpeed = _movement;
+        rotationSpeed = _rotation;
+    }
     public void SetTankController(TankController _tankController)
     {
         tankController = _tankController;
